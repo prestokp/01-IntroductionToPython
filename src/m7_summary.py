c6a5503ec92202c2756b54e27db0a7600e0bc424
prestokp <prestokp@rose-hulman.edu>
2018-11-26 12:30:07 -0500

    SpaceX enthusiast print. Done.

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Kirk Preston.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -43,3 +43,12 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window=rg.TurtleWindow()
+rg.SimpleTurtle()
+preston = rg.SimpleTurtle()
+preston.pen = rg.Pen('blue', 10)
+preston.forward(200)
+
+window.close_on_mouse_click()
+
